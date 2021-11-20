bca01c4b450962cbed8f19a5da82553cb79cfc6f
manish <mks19121999@gmail.com>
2021-11-21 01:43:44 +0530

    Added new enrollment page
    Signed-off-by: manish <mks19121999@gmail.com>

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,11 +3,18 @@ class EnrollmentsController < ApplicationController
   end
 
   def new
+    @enrollment = Enrollment.new
   end
 
   def create
+    @enrollment = Enrollment.create(params.require(:enrollment).permit(:course_id, :student_id))
+    redirect_to student_path(@enrollment.student)
   end
 
   def destroy
+    @enrollment = Enrollment.find(params[:id])
+    @student = @enrollment.student
+    @enrollment.destroy
+    redirect_to student_path(@student)
   end
 end
