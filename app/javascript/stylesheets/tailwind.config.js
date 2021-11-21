4c44d8de5fa03cde78540889bb62aa2e9a0c0678
manish <mks19121999@gmail.com>
2021-11-21 20:57:50 +0530

    Fixed UI
    Signed-off-by: manish <mks19121999@gmail.com>

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,11 +1,17 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sky: colors.sky,
+        cyan: colors.cyan,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
